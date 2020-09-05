59a9ac3807ecff8b069452c3c79b80ab4dbd4d46
manasa <lakshmimanasasai.t@gmail.com>
2020-09-06 02:19:11 +0530

    maincontent refactor

--- src/MainContent/index.tsx
+++ src/MainContent/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { Events } from '../Utils/types';
 
 import FilterContext from '../Context/filter';
@@ -12,10 +12,11 @@ type HeaderProps = {
     setFilterVisibility: (state: boolean) => void;
 };
 
-class MainContent extends Component<HeaderProps, {}> {
-    static contextType = FilterContext;
+const MainContent: React.FC<HeaderProps> = ({ currentEvents, currentView, updateAnEvent, setFilterVisibility }) => {
+    // context
+    const showFilter = useContext(FilterContext);
 
-    handleOnClick = (event: Events, state: boolean) => {
+    const handleOnClick = (event: Events, state: boolean) => {
         if (!state || window.confirm(`Confirm sign up for the event "${event.name}"`)) {
             fetch(`http://localhost:3001/events/${event.id}`, {
                 method: 'PATCH',
@@ -26,82 +27,81 @@ class MainContent extends Component<HeaderProps, {}> {
             })
                 .then((response) => response.json())
                 .then((response) => {
-                    this.props.updateAnEvent(response);
+                    updateAnEvent(response);
                 });
         }
     };
 
-    render() {
-        const events = this.props.currentEvents;
-        let hash: { [k: string]: Array<Events> } = {};
+    // Process display
+    let hash: { [k: string]: Array<Events> } = {};
 
-        events.forEach((event) => {
-            let date = event.startDate;
-            let dateKey: string = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
-            if (hash[dateKey]) {
-                hash[dateKey].push(event);
-            } else {
-                hash[dateKey] = [event];
-            }
-        });
+    currentEvents.forEach((event) => {
+        // group by date
+        let date = event.startDate;
+        let dateKey: string = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+        if (hash[dateKey]) {
+            hash[dateKey].push(event);
+        } else {
+            hash[dateKey] = [event];
+        }
+    });
 
-        let eventList = Object.entries(hash).map((arr) => {
-            let block = arr[1].map((event) => {
-                const startDate = event.startDate;
-                const endDate = event.endDate;
-                const duration = (endDate.getTime() - startDate.getTime()) / 60000;
-                const show = this.props.currentView === 'all' || (this.props.currentView === 'my' && event.isSignedUp);
-                if (!show) return null;
-
-                return (
-                    <div className="container" key={event.id}>
-                        <div className="primary">
-                            {event.isFree && <span className="free">FREE</span>}
-                            <h2 className="title">{event.name}</h2>
-                            {event.isSignedUp && this.props.currentView === 'my' && (
-                                <button className="button" onClick={(e) => this.handleOnClick(event, false)}>
-                                    Cancel
-                                </button>
-                            )}
-                            {!event.isSignedUp && (
-                                <button className="button" onClick={(e) => this.handleOnClick(event, true)}>
-                                    Sign up
-                                </button>
-                            )}
-                        </div>
-                        <div className="secondary">
-                            <p className="para">{event.city}</p>
-                            <p className="para">{duration}'</p>
-                            {/* todo: make 0:0 as 00:00 */}
-                            <p className="para">
-                                from {startDate.getHours()}:{startDate.getMinutes()} to {endDate.getHours()}:
-                                {endDate.getMinutes()}
-                            </p>
-                        </div>
-                    </div>
-                );
-            });
-            block = block.filter((block) => block);
-            if (!block.length) return null;
+    let eventList = Object.entries(hash).map((arr) => {
+        let block = arr[1].map((event) => {
+            const startDate = event.startDate;
+            const endDate = event.endDate;
+            const duration = (endDate.getTime() - startDate.getTime()) / 60000;
+            const show = currentView === 'all' || (currentView === 'my' && event.isSignedUp);
+            if (!show) return null;
 
             return (
-                <li key={arr[0]}>
-                    <p className="para">{arr[0]}</p>
-                    {block}
-                </li>
+                <div className="container" key={event.id}>
+                    <div className="primary">
+                        {event.isFree && <span className="free">FREE</span>}
+                        <h2 className="title">{event.name}</h2>
+                        {event.isSignedUp && currentView === 'my' && (
+                            <button className="button" onClick={(e) => handleOnClick(event, false)}>
+                                Cancel
+                            </button>
+                        )}
+                        {!event.isSignedUp && (
+                            <button className="button" onClick={(e) => handleOnClick(event, true)}>
+                                Sign up
+                            </button>
+                        )}
+                    </div>
+                    <div className="secondary">
+                        <p className="para">{event.city}</p>
+                        <p className="para">{duration}'</p>
+                        {/* todo: make 0:0 as 00:00 */}
+                        <p className="para">
+                            from {startDate.getHours()}:{startDate.getMinutes()} to {endDate.getHours()}:
+                            {endDate.getMinutes()}
+                        </p>
+                    </div>
+                </div>
             );
         });
-        eventList = eventList.filter((event) => event);
+        block = block.filter((block) => block);
+        if (!block.length) return null;
 
         return (
-            <section className={`${this.context ? 'hide' : ''}`} id="main-content">
-                <button className="button hidden-mobile-button" onClick={(e) => this.props.setFilterVisibility(true)}>
-                    Filters
-                </button>
-                {eventList.length ? eventList : <div>This view has no events</div>}
-            </section>
+            <li key={arr[0]}>
+                <p className="para">{arr[0]}</p>
+                {block}
+            </li>
         );
-    }
-}
+    });
+    eventList = eventList.filter((event) => event);
+
+    return (
+        <section className={`${showFilter ? 'hide' : ''}`} id="main-content">
+            <button className="button hidden-mobile-button" onClick={(e) => setFilterVisibility(true)}>
+                Filters
+            </button>
+            {eventList.length ? eventList : <div>This view has no events</div>}
+        </section>
+    );
+};
 
 export default MainContent;
