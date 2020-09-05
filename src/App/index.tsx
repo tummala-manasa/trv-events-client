5fa78654a8fe6a6235115169e3dc6653951cfb03
manasa <lakshmimanasasai.t@gmail.com>
2020-09-05 22:46:17 +0530

    header component refactor

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -18,10 +18,10 @@ class App extends Component<{}, AppState> {
     constructor(props: Object) {
         super(props);
         this.state = {
-            events: [],
-            currentEvents: [],
+            events: [], // all events
+            currentEvents: [], // events displayed on screen
             currentView: 'all',
-            showFilters: false,
+            showFilters: false, // toggle button in mobile view
         };
     }
 
@@ -29,7 +29,7 @@ class App extends Component<{}, AppState> {
         const citiesAPI = fetch('http://localhost:3001/cities');
         const eventsAPI = fetch('http://localhost:3001/events');
 
-        Promise.all([citiesAPI, eventsAPI])
+        Promise.all([citiesAPI, eventsAPI]) // make parallel calls
             .then((response) => Promise.all([response[0].json(), response[1].json()]))
             .then((response) => {
                 let cities: Array<City> = response[0];
@@ -39,8 +39,8 @@ class App extends Component<{}, AppState> {
                     let city: City | undefined = cities.find((city) => city.id === event.city);
 
                     return Object.assign({}, event, {
-                        city: city?.name,
-                        startDate: new Date(event.startDate),
+                        city: city?.name, // replacing city ID with name
+                        startDate: new Date(event.startDate), // converting string to date
                         endDate: new Date(event.endDate),
                     });
                 });
@@ -61,13 +61,16 @@ class App extends Component<{}, AppState> {
     };
 
     updateAnEvent = (event: Events) => {
+        // After post call for sign up, we manually update events
         this.setState((prevState) => {
+            // Updating events
             let newEvents = [...prevState.events];
             let tempEventInAll = newEvents.find((evt) => evt.id === event.id);
             if (tempEventInAll) {
                 tempEventInAll.isSignedUp = event.isSignedUp;
             }
 
+            // Updating current events
             let newCurrentEvents = [...prevState.currentEvents];
             let tempEventInCur = newCurrentEvents.find((evt) => evt.id === event.id);
             if (tempEventInCur) {
@@ -86,7 +89,7 @@ class App extends Component<{}, AppState> {
         const events: Array<Events> = this.state.events;
         return (
             <>
-                <Header events={events} setCurrentView={this.setCurrentView} currentView={this.state.currentView} />
+                <Header setCurrentView={this.setCurrentView} currentView={this.state.currentView} />
                 <Filters
                     events={events}
                     setCurrentEvents={this.setCurrentEvents}
--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,40 +1,35 @@
-import React, { Component } from 'react';
-import { Events } from '../Utils/types';
+import React from 'react';
 
 import './index.css';
 
 type HeaderProps = {
-    events: Array<Events>;
     currentView: string;
     setCurrentView: (v: string) => void;
 };
 
-class Header extends Component<HeaderProps, {}> {
-    render() {
-        const currentView: string = this.props.currentView;
-        return (
-            <header className="header">
-                TRIVAGO
-                <span className="place">
-                    <button
-                        className={`button anchor-button ${currentView === 'all' ? 'active' : ''}`}
-                        onClick={(e) => this.props.setCurrentView('all')}
-                    >
-                        All events
-                    </button>
-                    <button
-                        className={`button anchor-button ${currentView === 'my' ? 'active' : ''}`}
-                        onClick={(e) => this.props.setCurrentView('my')}
-                    >
-                        My events
-                    </button>
-                    <a href="/" className="link">
-                        About
-                    </a>
-                </span>
-            </header>
-        );
-    }
-}
+const Header: React.FC<HeaderProps> = ({ currentView, setCurrentView }) => {
+    return (
+        <header className="header">
+            TRIVAGO
+            <span className="place">
+                <button
+                    className={`button anchor-button ${currentView === 'all' ? 'active' : ''}`}
+                    onClick={(e) => setCurrentView('all')}
+                >
+                    All events
+                </button>
+                <button
+                    className={`button anchor-button ${currentView === 'my' ? 'active' : ''}`}
+                    onClick={(e) => setCurrentView('my')}
+                >
+                    My events
+                </button>
+                <a href="/" className="link">
+                    About
+                </a>
+            </span>
+        </header>
+    );
+};
 
 export default Header;
