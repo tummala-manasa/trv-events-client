6fe186dde4d602c727059af11c53e4b3de1a77a6
manasa <lakshmimanasasai.t@gmail.com>
2020-09-07 01:05:14 +0530

    added new component

---
+++ src/EventContainer/index.tsx
@@ -0,0 +1,59 @@
+import React, { useContext, useState } from 'react';
+import { Events } from '../Utils/types';
+
+import './index.css';
+
+type ContainerProps = {
+    event: Events;
+    currentView: string;
+    handleOnClick: (event: Events, state: boolean) => void;
+    setIsModalOpen: (s: boolean) => void;
+    setModalEvent: (e: Events) => void;
+};
+
+const EventContainer: React.FC<ContainerProps> = ({
+    event,
+    currentView,
+    handleOnClick,
+    setModalEvent,
+    setIsModalOpen,
+}) => {
+    const startDate = event.startDate;
+    const endDate = event.endDate;
+    const duration = (endDate.getTime() - startDate.getTime()) / 60000;
+
+    return (
+        <div className="container" key={event.id}>
+            <div className="primary">
+                {event.isFree && <span className="free">FREE</span>}
+                <h2 className="title">{event.name}</h2>
+                {event.isSignedUp && currentView === 'my' && (
+                    <button className="button" onClick={(e) => handleOnClick(event, false)}>
+                        Cancel
+                    </button>
+                )}
+                {!event.isSignedUp && (
+                    <button
+                        className="button"
+                        onClick={(e) => {
+                            setIsModalOpen(true);
+                            setModalEvent(event);
+                        }}
+                    >
+                        Sign up
+                    </button>
+                )}
+            </div>
+            <div className="secondary">
+                <p className="para">{event.city}</p>
+                <p className="para">{duration}'</p>
+                {/* todo: make 0:0 as 00:00 */}
+                <p className="para">
+                    from {startDate.getHours()}:{startDate.getMinutes()} to {endDate.getHours()}:{endDate.getMinutes()}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default EventContainer;
--- src/MainContent/index.tsx
+++ src/MainContent/index.tsx
@@ -3,6 +3,7 @@ import { Events } from '../Utils/types';
 
 import FilterContext from '../Context/filter';
 import Modal from '../Modal';
+import EventContainer from '../EventContainer';
 
 import './index.css';
 
@@ -51,44 +52,18 @@ const MainContent: React.FC<HeaderProps> = ({ currentEvents, currentView, update
 
     let eventList = Object.entries(hash).map((arr) => {
         let block = arr[1].map((event) => {
-            const startDate = event.startDate;
-            const endDate = event.endDate;
-            const duration = (endDate.getTime() - startDate.getTime()) / 60000;
             const show = currentView === 'all' || (currentView === 'my' && event.isSignedUp);
             if (!show) return null;
 
             return (
-                <div className="container" key={event.id}>
-                    <div className="primary">
-                        {event.isFree && <span className="free">FREE</span>}
-                        <h2 className="title">{event.name}</h2>
-                        {event.isSignedUp && currentView === 'my' && (
-                            <button className="button" onClick={(e) => handleOnClick(event, false)}>
-                                Cancel
-                            </button>
-                        )}
-                        {!event.isSignedUp && (
-                            <button
-                                className="button"
-                                onClick={(e) => {
-                                    setIsModalOpen(true);
-                                    setModalEvent(event);
-                                }}
-                            >
-                                Sign up
-                            </button>
-                        )}
-                    </div>
-                    <div className="secondary">
-                        <p className="para">{event.city}</p>
-                        <p className="para">{duration}'</p>
-                        {/* todo: make 0:0 as 00:00 */}
-                        <p className="para">
-                            from {startDate.getHours()}:{startDate.getMinutes()} to {endDate.getHours()}:
-                            {endDate.getMinutes()}
-                        </p>
-                    </div>
-                </div>
+                <EventContainer
+                    key={event.id}
+                    event={event}
+                    handleOnClick={handleOnClick}
+                    currentView={currentView}
+                    setModalEvent={setModalEvent}
+                    setIsModalOpen={setIsModalOpen}
+                />
             );
         });
         block = block.filter((block) => block);
