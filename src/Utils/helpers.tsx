604b047d8ab5c58752a9f05c758be6512e70efc6
manasa <lakshmimanasasai.t@gmail.com>
2020-09-07 12:27:42 +0530

    logic

--- src/Utils/helpers.tsx
+++ src/Utils/helpers.tsx
@@ -1,21 +1,22 @@
 import { checkboxState } from '../Utils/types';
 
 const checkSlot = (state: checkboxState, date: Date) => {
-    const staringHours = date.getHours(); //todo: use miutes as well
+    const staringHours = date.getHours();
+
     const { isMorningChecked, isAfternoonChecked, isEveningChecked, isNightChecked } = state;
     let slot = false;
     if (isMorningChecked || isAfternoonChecked || isEveningChecked || isNightChecked) {
         if (isMorningChecked) {
-            slot = staringHours >= 6 && staringHours <= 12;
+            slot = staringHours >= 6 && staringHours <= 11;
         }
         if (isAfternoonChecked) {
-            slot = slot || (staringHours > 12 && staringHours <= 17);
+            slot = slot || (staringHours >= 12 && staringHours <= 16);
         }
         if (isEveningChecked) {
-            slot = slot || (staringHours > 17 && staringHours <= 21);
+            slot = slot || (staringHours >= 17 && staringHours <= 20);
         }
         if (isNightChecked) {
-            slot = slot || staringHours > 21 || staringHours < 6;
+            slot = slot || staringHours >= 21 || staringHours < 6;
         }
     } else {
         slot = true;
